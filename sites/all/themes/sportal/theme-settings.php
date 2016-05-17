fa185ab37f7df3401f719a37c393f4167f0f2bba
Fallenstedt <a.fallenstedt@gmail.com>
2016-05-17 09:16:43 -0700

    correct theme-settings.php

--- sites/all/themes/sportal/theme-settings.php
+++ sites/all/themes/sportal/theme-settings.php
@@ -7,99 +7,58 @@
  * @param $form_state
  *   The form state.
  */
-function sportal_form_system_theme_settings_alter(&$form, &$form_state) {
+function nexus_form_system_theme_settings_alter(&$form, &$form_state) {
 
-  $form['sportal_settings'] = array(
+  $form['nexus_settings'] = array(
     '#type' => 'fieldset',
     '#title' => t('Nexus Settings'),
     '#collapsible' => FALSE,
     '#collapsed' => FALSE,
   );
-  $form['sportal_settings']['breadcrumbs'] = array(
+  $form['nexus_settings']['breadcrumbs'] = array(
     '#type' => 'checkbox',
     '#title' => t('Show breadcrumbs in a page'),
-    '#default_value' => theme_get_setting('breadcrumbs','sportal'),
+    '#default_value' => theme_get_setting('breadcrumbs','nexus'),
     '#description'   => t("Check this option to show breadcrumbs in page. Uncheck to hide."),
   );
-  $form['sportal_settings']['slideshow'] = array(
+  $form['nexus_settings']['slideshow'] = array(
     '#type' => 'fieldset',
     '#title' => t('Front Page Slideshow'),
     '#collapsible' => TRUE,
     '#collapsed' => FALSE,
   );
-  $form['sportal_settings']['slideshow']['slideshow_display'] = array(
+  $form['nexus_settings']['slideshow']['slideshow_display'] = array(
     '#type' => 'checkbox',
     '#title' => t('Show slideshow'),
-    '#default_value' => theme_get_setting('slideshow_display','sportal'),
+    '#default_value' => theme_get_setting('slideshow_display','nexus'),
     '#description'   => t("Check this option to show Slideshow in front page. Uncheck to hide."),
   );
-  $form['sportal_settings']['slideshow']['slide'] = array(
+  $form['nexus_settings']['slideshow']['slide'] = array(
     '#markup' => t('You can change the description and URL of each slide in the following Slide Setting fieldsets.'),
   );
-  $form['sportal_settings']['slideshow']['slide1'] = array(
+  $form['nexus_settings']['slideshow']['slide1'] = array(
     '#type' => 'fieldset',
     '#title' => t('Slide 1'),
     '#collapsible' => TRUE,
     '#collapsed' => TRUE,
   );
-  $form['sportal_settings']['slideshow']['slide1']['slide1_head'] = array(
+  $form['nexus_settings']['slideshow']['slide1']['slide1_head'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide Headline'),
-    '#default_value' => theme_get_setting('slide1_head','sportal'),
+    '#default_value' => theme_get_setting('slide1_head','nexus'),
   );
-  $form['sportal_settings']['slideshow']['slide1']['slide1_desc'] = array(
+  $form['nexus_settings']['slideshow']['slide1']['slide1_desc'] = array(
     '#type' => 'textarea',
     '#title' => t('Slide Description'),
-    '#default_value' => theme_get_setting('slide1_desc','sportal'),
+    '#default_value' => theme_get_setting('slide1_desc','nexus'),
   );
-  $form['sportal_settings']['slideshow']['slide1']['slide1_url'] = array(
+  $form['nexus_settings']['slideshow']['slide1']['slide1_url'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide URL'),
-    '#default_value' => theme_get_setting('slide1_url','sportal'),
+    '#default_value' => theme_get_setting('slide1_url','nexus'),
   );
-  $form['sportal_settings']['slideshow']['slide2'] = array(
-    '#type' => 'fieldset',
-    '#title' => t('Slide 2'),
-    '#collapsible' => TRUE,
-    '#collapsed' => TRUE,
-  );
-  $form['sportal_settings']['slideshow']['slide2']['slide2_head'] = array(
-    '#type' => 'textfield',
-    '#title' => t('Slide Headline'),
-    '#default_value' => theme_get_setting('slide2_head','sportal'),
-  );
-  $form['sportal_settings']['slideshow']['slide2']['slide2_desc'] = array(
-    '#type' => 'textarea',
-    '#title' => t('Slide Description'),
-    '#default_value' => theme_get_setting('slide2_desc','sportal'),
-  );
-  $form['sportal_settings']['slideshow']['slide2']['slide2_url'] = array(
-    '#type' => 'textfield',
-    '#title' => t('Slide URL'),
-    '#default_value' => theme_get_setting('slide2_url','sportal'),
-  );
-  $form['sportal_settings']['slideshow']['slide3'] = array(
-    '#type' => 'fieldset',
-    '#title' => t('Slide 3'),
-    '#collapsible' => TRUE,
-    '#collapsed' => TRUE,
-  );
-  $form['sportal_settings']['slideshow']['slide3']['slide3_head'] = array(
-    '#type' => 'textfield',
-    '#title' => t('Slide Headline'),
-    '#default_value' => theme_get_setting('slide3_head','sportal'),
-  );
-  $form['sportal_settings']['slideshow']['slide3']['slide3_desc'] = array(
-    '#type' => 'textarea',
-    '#title' => t('Slide Description'),
-    '#default_value' => theme_get_setting('slide3_desc','sportal'),
-  );
-  $form['sportal_settings']['slideshow']['slide3']['slide3_url'] = array(
-    '#type' => 'textfield',
-    '#title' => t('Slide URL'),
-    '#default_value' => theme_get_setting('slide3_url','sportal'),
-  );
-  $form['sportal_settings']['slideshow']['slideimage'] = array(
-    '#markup' => t('To change the Slide Images, Replace the slide-image-1.jpg, slide-image-2.jpg and slide-image-3.jpg in the images folder of the theme folder.'),
+
+  $form['nexus_settings']['slideshow']['slideimage'] = array(
+    '#markup' => t('To change the Slide Image, Replace the slide-image-1.jpg in the images folder of the theme folder.'),
   );
 }
